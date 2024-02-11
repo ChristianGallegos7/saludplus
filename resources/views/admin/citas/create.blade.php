7eb990cdca4e488fdb92e2a0bf55aaffb014f569
Sakayanagi <sakayanagi@gmail.com>
2024-02-11 18:56:15 -0500

    nuevas cositas

--- resources/views/admin/citas/create.blade.php
+++ resources/views/admin/citas/create.blade.php
@@ -12,110 +12,115 @@
     <div class="flex justify-center p-4">
 
         <div class="w-4/12 p-4 bg-gray-200">
+
             <!-- Formulario para crear una nueva cita -->
-            <h2 class="text-2xl font-bold mb-4">Crear Nueva Cita</h2>
+            <h2 class="text-2xl font-bold mb-4 text-center mt-3">Crear Nueva Cita</h2>
             <!-- Formulario para crear una nueva cita -->
-<form action="{{ route('admin.store.appointment') }}" method="post" novalidate>
-    @csrf
-
-    <div class="mb-4">
-        <label for="appointment_datetime" class="block text-sm font-semibold text-gray-600">Fecha y Hora de la Cita:</label>
-        <input type="datetime-local" id="appointment_datetime" name="appointment_datetime" required
-            min="{{ now()->format('Y-m-d\TH:i') }}"
-            class="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-            value="{{ old('appointment_datetime') }}">
-        @error('appointment_datetime')
-            <p class="bg-red-600 text-white uppercase p-3 rounded-lg text-center mt-2 text-sm">
-                {{ $message }}
-            </p>
-        @enderror
-    </div>
-
-    <div class="mb-4">
-        <label for="specialty" class="block text-sm font-semibold text-gray-600">Especialidad:</label>
-        <select id="specialty" name="specialty" required class="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring focus:border-blue-300">
-            <option value="">Selecciona una especialidad</option>
-            @foreach ($specialties as $specialty)
-                <option value="{{ $specialty->id }}">{{ $specialty->name }}</option>
-            @endforeach
-        </select>
-        @error('specialty')
-            <p class="bg-red-600 text-white uppercase p-3 rounded-lg text-center mt-2 text-sm">
-                {{ $message }}
-            </p>
-        @enderror
-    </div>
+            <form action="{{ route('admin.store.appointment') }}" method="post" novalidate>
+                @csrf
+                <fieldset>
+                    <legend class="text-lg font-semibold text-gray-800 bg-yellow-300 p-1">Atención desde las 8:00 a 17:00</legend>
+                    <div class="mb-4">
+                        <label for="appointment_datetime" class="block text-sm font-semibold text-gray-600 mt-1">Fecha y Hora de la
+                            Cita:</label>
+                        <input type="datetime-local" id="appointment_datetime" name="appointment_datetime" required
+                            min="{{ now()->format('Y-m-d\TH:i') }}"
+                            class="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+                            value="{{ old('appointment_datetime') }}">
+                        @error('appointment_datetime')
+                            <p class="bg-red-600 text-white uppercase p-3 rounded-lg text-center mt-2 text-sm">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
     
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-    <script>
-        document.getElementById('specialty').addEventListener('change', function() {
-            var specialtyId = this.value;
-            axios.get('/get-doctors-by-specialty', {
-                params: {
-                    specialty_id: specialtyId
-                }
-            })
-            .then(function(response) {
-                // Manejar la respuesta y actualizar la lista de médicos en el campo de selección de médicos
-            })
-            .catch(function(error) {
-                console.error('Error al obtener los médicos:', error);
-            });
-        });
-    </script>
+                    <div class="mb-4">
+                        <label for="specialty" class="block text-sm font-semibold text-gray-600">Especialidad:</label>
+                        <select id="specialty" name="specialty" required
+                            class="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring focus:border-blue-300">
+                            <option value="">Selecciona una especialidad</option>
+                            @foreach ($specialties as $specialty)
+                                <option value="{{ $specialty->id }}">{{ $specialty->name }}</option>
+                            @endforeach
+                        </select>
+                        @error('specialty')
+                            <p class="bg-red-600 text-white uppercase p-3 rounded-lg text-center mt-2 text-sm">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
     
+                    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+                    <script>
+                        document.getElementById('specialty').addEventListener('change', function() {
+                            var specialtyId = this.value;
+                            axios.get('/get-doctors-by-specialty', {
+                                    params: {
+                                        specialty_id: specialtyId
+                                    }
+                                })
+                                .then(function(response) {
+                                    // Manejar la respuesta y actualizar la lista de médicos en el campo de selección de médicos
+                                })
+                                .catch(function(error) {
+                                    console.error('Error al obtener los médicos:', error);
+                                });
+                        });
+                    </script>
+    
+    
+                    <div class="mb-4">
+                        <label for="doctor_id" class="block text-sm font-semibold text-gray-600">Médico Asociado:</label>
+                        <select id="doctor_id" name="doctor_id" required
+                            class="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring focus:border-blue-300">
+                            <!-- Iterar sobre la lista de doctores para mostrar las opciones -->
+                            @foreach ($doctors as $doctor)
+                                <option value="{{ $doctor->id }}">{{ $doctor->nombre }}</option>
+                            @endforeach
+                        </select>
+                        @error('doctor_id')
+                            <p class="bg-red-600 text-white uppercase p-3 rounded-lg text-center mt-2 text-sm">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+    
+                    <div class="mb-4">
+                        <label for="additional_info" class="block text-sm font-semibold text-gray-600">Información
+                            Adicional:</label>
+                        <textarea id="additional_info" name="additional_info"
+                            class="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring focus:border-blue-300">{{ old('additional_info') }}</textarea>
+                        @error('additional_info')
+                            <p class="bg-red-600 text-white uppercase p-3 rounded-lg text-center mt-2 text-sm">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+    
+                    <div class="mb-4">
+                        <label for="status" class="block text-sm font-semibold text-gray-600">Estado de la Cita:</label>
+                        <select id="status" name="status" required
+                            class="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring focus:border-blue-300">
+                            <option value="available">Disponible</option>
+                            <option value="reserved">Reservada</option>
+                            <option value="completed">Completada</option>
+                        </select>
+                        @error('status')
+                            <p class="bg-red-600 text-white uppercase p-3 rounded-lg text-center mt-2 text-sm">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+    
+                    <button type="submit"
+                        class="w-full p-2 font-bold bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none">
+                        Crear Cita Médica
+                    </button>
+                </fieldset>
 
-    <div class="mb-4">
-        <label for="doctor_id" class="block text-sm font-semibold text-gray-600">Médico Asociado:</label>
-        <select id="doctor_id" name="doctor_id" required
-            class="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring focus:border-blue-300">
-            <!-- Iterar sobre la lista de doctores para mostrar las opciones -->
-            @foreach ($doctors as $doctor)
-                <option value="{{ $doctor->id }}">{{ $doctor->nombre }}</option>
-            @endforeach
-        </select>
-        @error('doctor_id')
-            <p class="bg-red-600 text-white uppercase p-3 rounded-lg text-center mt-2 text-sm">
-                {{ $message }}
-            </p>
-        @enderror
-    </div>
-
-    <div class="mb-4">
-        <label for="additional_info" class="block text-sm font-semibold text-gray-600">Información Adicional:</label>
-        <textarea id="additional_info" name="additional_info"
-            class="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring focus:border-blue-300">{{ old('additional_info') }}</textarea>
-        @error('additional_info')
-            <p class="bg-red-600 text-white uppercase p-3 rounded-lg text-center mt-2 text-sm">
-                {{ $message }}
-            </p>
-        @enderror
-    </div>
-
-    <div class="mb-4">
-        <label for="status" class="block text-sm font-semibold text-gray-600">Estado de la Cita:</label>
-        <select id="status" name="status" required
-            class="w-full p-2 mt-1 border rounded-md focus:outline-none focus:ring focus:border-blue-300">
-            <option value="available">Disponible</option>
-            <option value="reserved">Reservada</option>
-            <option value="completed">Completada</option>
-        </select>
-        @error('status')
-            <p class="bg-red-600 text-white uppercase p-3 rounded-lg text-center mt-2 text-sm">
-                {{ $message }}
-            </p>
-        @enderror
-    </div>
-
-    <button type="submit"
-        class="w-full p-2 font-bold bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none">
-        Crear Cita Médica
-    </button>
-</form>
+               
+            </form>
 
         </div>
     </div>
 @endsection
-
-
-
